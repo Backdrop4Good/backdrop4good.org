9fa5241ff5053c2a65858469c6352691d26b137d
Geoff St. Pierre <serundeputy@gmail.com>
2017-09-09 10:44:11 -0400

    Adding platform include statements.

--- web/settings.php
+++ web/settings.php
@@ -150,17 +150,18 @@ $settings['404_fast_html'] = '<!DOCTYPE html><html><head><title>404 Not Found</t
 $settings['backdrop_drupal_compatibility'] = TRUE;
 
 ////////////////// PLATFORM //////////////////////
-// /**
-//  * @file
-//  * Platform.sh example settings.php file for Drupal 7.
-//  */
-// // Recommended PHP settings.
-// ini_set('session.gc_probability', 1);
-// ini_set('session.gc_divisor', 100);
-// ini_set('session.gc_maxlifetime', 200000);
-// ini_set('session.cookie_lifetime', 2000000);
-// ini_set('pcre.backtrack_limit', 200000);
-// ini_set('pcre.recursion_limit', 200000);
+/**
+ * @file
+ * Platform.sh example settings.php file for Drupal 7.
+ */
+// Recommended PHP settings.
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+ini_set('session.gc_maxlifetime', 200000);
+ini_set('session.cookie_lifetime', 2000000);
+ini_set('pcre.backtrack_limit', 200000);
+ini_set('pcre.recursion_limit', 200000);
+
 // // Default Drupal 7 settings.
 // //
 // // These are already explained with detailed comments in Drupal's
@@ -172,12 +173,13 @@ $settings['backdrop_drupal_compatibility'] = TRUE;
 // $drupal_hash_salt = '';
 // // Set Drupal not to check for HTTP connectivity.
 // $conf['drupal_http_request_fails'] = FALSE;
+
 // // Include automatic Platform.sh settings.
-// if (file_exists(__DIR__ . '/settings.platformsh.php')) {
-//   require_once(__DIR__ . '/settings.platformsh.php');
-// }
-// // Include local settings. These come last so that they can override anything.
-// $on_platformsh = !empty($_ENV['PLATFORM_PROJECT']);
-// if (file_exists(__DIR__ . '/settings.local.php') && !$on_platformsh) {
-//   require_once(__DIR__ . '/settings.local.php');
-// }
+if (file_exists(__DIR__ . '/settings.platformsh.php')) {
+  require_once(__DIR__ . '/settings.platformsh.php');
+}
+// Include local settings. These come last so that they can override anything.
+$on_platformsh = !empty($_ENV['PLATFORM_PROJECT']);
+if (file_exists(__DIR__ . '/settings.local.php') && !$on_platformsh) {
+  require_once(__DIR__ . '/settings.local.php');
+}
