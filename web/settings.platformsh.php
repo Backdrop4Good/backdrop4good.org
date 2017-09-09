c2c2332c61ebe7a46ab3fd4f2a79f1f148959120
Geoff St. Pierre <serundeputy@gmail.com>
2017-09-09 11:24:00 -0400

    another platform settings itteration.

--- web/settings.platformsh.php
+++ web/settings.platformsh.php
@@ -1,4 +1,10 @@
 <?php
+// DB Connection.
+// $DB_USER = getenv('DB_USER');
+// $DB_PASS = getenv('DB_PASS');
+// $DB_NAME = getenv('DB_NAME');
+$database = "mysql://main:@localhost/main";
+
 // Point to Backdrop config.
 $config_directories['active'] = '../config/active';
 $config_directories['staging'] = '../config/staging';
